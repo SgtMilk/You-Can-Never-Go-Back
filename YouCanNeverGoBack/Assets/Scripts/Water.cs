9fd9e5894fccbda6a12f0ac24e4059936991f0f9
lplb <lplb@outlook.com>
2020-01-10 19:48:55 -0500

    ajout script basique pour l'eau

---
+++ YouCanNeverGoBack/Assets/Scripts/Water.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public float floodingRate = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().localScale += new Vector3(0, floodingRate * Time.deltaTime, 0);
+    }
+}
