da7a79fa281cf87b8eece168304a454366118d9e
lplb <lplb@outlook.com>
2020-01-10 23:35:20 -0500

    ajout de boite a pousser

--- YouCanNeverGoBack/Assets/Scripts/Moe.cs
+++ YouCanNeverGoBack/Assets/Scripts/Moe.cs
@@ -6,7 +6,12 @@ public class Moe : MonoBehaviour
 {
     public float walkingSpeed = 100.0f;
     public float jumpSpeed = 250.0f;
+    public float pushForce = 100.0f;
     public LayerMask groundLayer;
+    public LayerMask pushableLayer;
+
+    private bool isPushingObject = false;
+    private GameObject pushedObject;
     private List<int> keys = new List<int>();
 
     // Start is called before the first frame update
@@ -23,6 +28,15 @@ public class Moe : MonoBehaviour
         {
             controller.velocity = new Vector2(Input.GetAxis("Horizontal") == 0 ? 0 : Time.deltaTime * walkingSpeed * Input.GetAxis("Horizontal"),
                                               Input.GetAxis("Jump") == 0 || !isGrounded() ? controller.velocity.y : Time.deltaTime * jumpSpeed);
+        }
+
+        if (isPushingObject && pushedObject)
+        {
+            Rigidbody2D objectRigidBody = pushedObject.GetComponent<Rigidbody2D>();
+            if (objectRigidBody)
+            {
+                objectRigidBody.AddForce(controller.velocity.normalized * pushForce);
+            }
         }
     }
 
@@ -45,4 +59,22 @@ public class Moe : MonoBehaviour
         }
         return false;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == pushableLayer)
+        {
+            pushedObject = collision.gameObject;
+            isPushingObject = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == pushableLayer)
+        {
+            isPushingObject = false;
+        }
+    }
+
 }
